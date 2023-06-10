bcf830632a4f68f32ca2f09cf23096bed8320fc9
diodorusca <diodorusca@student.uns.ac.id>
2023-06-10 18:02:54 +0700

    1

--- wisata&penginapan.py
+++ wisata&penginapan.py
@@ -1,11 +1,31 @@
 import csv
 print("===="*20)
-def buka_wisata(wisata):
-    with open('datakost.txt') as csv_file:
+def buka_wisata(hotel):
+    with open("List_hotel.txt") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for i in csv_reader:
-            if i[1] == wisata:
-                print(f"Nama Tempat Wisata: {i[1]} dengan tiket masuk: {i[0]} memiliki rating: {i[2]}")
+            if i[0] == hotel:
+                print(f'''Nama Hotel: {i[1]}, dengan Harga/malam: {i[0]}, memiliki rating: {i[2]}''')
             return True
         return False
-           
+
+def main():
+    hari=input("hari: ")
+    print('''silakan memilih rentang budget anda: 
+            1. < Rp 600.000,-
+            2. > RP 600.000,-''')
+    budget=input("budget (1/2): ")
+    with open("List_hotel.txt") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        daftar_hotel=[]
+
+        if hari == 2:
+            if budget == 1:
+                for i in csv_reader:
+                        if int(i[0]) < 250000  :
+                            daftar_hotel.append(i)
+                            print(f'{i[0]}. {i[1]}')
+
+if __name__ == "__main__":
+    main()
