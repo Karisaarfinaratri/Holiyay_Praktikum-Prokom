d61898a82e1ca6aa5e3207dc4e6ba5f9b0632211
diodorusca <diodorusca@student.uns.ac.id>
2023-06-10 19:01:52 +0700

    1

--- wisata&penginapan.py
+++ wisata&penginapan.py
@@ -1,31 +1,85 @@
 import csv
-print("===="*20)
-def buka_wisata(hotel):
-    with open("List_hotel.txt") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for i in csv_reader:
-            if i[0] == hotel:
-                print(f'''Nama Hotel: {i[1]}, dengan Harga/malam: {i[0]}, memiliki rating: {i[2]}''')
-            return True
-        return False
 
 def main():
-    hari=input("hari: ")
-    print('''silakan memilih rentang budget anda: 
-            1. < Rp 600.000,-
-            2. > RP 600.000,-''')
-    budget=input("budget (1/2): ")
-    with open("List_hotel.txt") as csv_file:
+    print(
+    '''Silakan memilih jumlah hari:
+    1. 2 hari
+    2. 3 hari
+    3. 4 hari''')
+    hari=input("hari (1/2/3): ")
+    with open("D:/Matkul Prokom/TUBES PROKOM/Holiyay_Praktikum-Prokom/List_Hotel.txt") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
 
-        daftar_hotel=[]
-
-        if hari == 2:
-            if budget == 1:
+        if hari == "1":
+            daftar_hotel=[]
+            print('''silakan memilih rentang budget anda: 
+            1. < Rp 600.000,-
+            2. > RP 600.000,-''')
+            budget=input("budget (1/2): ")
+            if budget == "1":
                 for i in csv_reader:
-                        if int(i[0]) < 250000  :
+                        if int(i[0]) < 230000  :
                             daftar_hotel.append(i)
-                            print(f'{i[0]}. {i[1]}')
+                            print(f'''
+Nama Hotel: {i[2]}
+Harga/malam: {i[1]}
+Rating: {i[3]}''')
+            elif budget == "2":
+                 for i in csv_reader:
+                        if int(i[0]) > 230000  :
+                            daftar_hotel.append(i)
+                            print(f'''
+Nama Hotel: {i[2]}
+Harga/malam: {i[1]}
+Rating: {i[3]}''')
+
+        elif hari == "2":
+            daftar_hotel=[]
+            print('''silakan memilih rentang budget anda: 
+            1. < Rp 800.000,-
+            2. > RP 800.000,-''')
+            budget=input("budget (1/2): ")
+            if budget == "1":
+                for i in csv_reader:
+                        if int(i[0]) < 220000  :
+                            daftar_hotel.append(i)
+                            print(f'''
+Nama Hotel: {i[2]}
+Harga/malam: {i[1]}
+Rating: {i[3]}''')                            
+            elif budget == "2":
+                 for i in csv_reader:
+                        if int(i[0]) > 220000  :
+                            daftar_hotel.append(i)
+                            print(f'''
+Nama Hotel: {i[2]}
+Harga/malam: {i[1]}
+Rating: {i[3]}''')
+         
+        elif hari == "3":
+            daftar_hotel=[]
+            print('''silakan memilih rentang budget anda: 
+            1. < Rp 1.000.000,-
+            2. > RP 1.000.000,-''')
+            budget=input("budget (1/2): ")
+            if budget == "1":
+                for i in csv_reader:
+                        if int(i[0]) < 220000  :
+                            daftar_hotel.append(i)
+                            print(f'''
+Nama Hotel: {i[2]}
+Harga/malam: {i[1]}
+Rating: {i[3]}''')
+            elif budget == "2":
+                 for i in csv_reader:
+                        if int(i[0]) > 220000  :
+                            daftar_hotel.append(i)
+                            print(f'''
+Nama Hotel: {i[2]}
+Harga/malam: {i[1]}
+Rating: {i[3]}''')
+
+            
 
 if __name__ == "__main__":
     main()
